04a0808ba8727557c95887b0c64fb6caf0d5546b
maximetourzel <max.tourzel@outlook.fr>
2023-03-04 18:59:20 +0100

    Add github action workflow

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  app: { pageTransition: { name: 'page', mode: 'out-in' } },
+  app: {
+    pageTransition: {
+      name: 'page',
+      mode: 'out-in'
+    }
+  },
   css: [
     'vuetify/lib/styles/main.sass',
     '@mdi/font/css/materialdesignicons.min.css'
